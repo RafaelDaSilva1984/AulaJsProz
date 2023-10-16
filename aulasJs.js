4e6bfd82e67522f049b5819d57c14cc410b7580a
Rafael da Silva <rfsrafaelsilva@gmail.com>
2023-10-16 15:42:40 -0300

    aula de ja var-let-const

---
+++ aulasJs.js
@@ -0,0 +1,17 @@
+console.log("Aula de Diferenciação entre Var, Let e Const");
+    
+
+if (true){
+    let num = 10
+    console.log("Declarando Variáveis com Let", num)
+}
+if (true){
+    var num = 10
+    
+}
+console.log("Declarando Variáveis com Var =", num)
+
+if (true){
+    const num = 10
+    console.log(`Declarando Variáveis com Constante const`, num)
+}
