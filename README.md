ba94d9af23eaf8ffe91d26639957ddfdc824bd57
Rafael da Silva <rfsrafaelsilva@gmail.com>
2023-10-18 09:41:08 -0300

    Alterado nome arquivo

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Aulas Js Proz Educação
 
 Módulo I, conhecendo o Js.
 
-Tipos de Variáveis e suas declarações: 
+Tipos de Variáveis e suas declarações:
 
 var - visível no escopo global
 
 let - visível no escopo local
 
 const - visível no escopo local
+
+Atalho para executar no terminal : ctrl + alt + n
---
+++ aula01Js.js
@@ -0,0 +1,18 @@
+console.log("Aula de Diferenciação entre Var, Let e Const");
+    
+//Exemplo de como declarar let
+if (true){
+    let num = 10
+    console.log("Declarando Variáveis com Let = ", num)
+}
+//Exemplo de como declarar var
+if (true){
+    var num = 10    
+}
+console.log("Declarando Variáveis com Var = ", num)
+
+//Exemplo de como declarar const
+if (true){
+    const num = 10
+    console.log(`Declarando Variáveis com Constante const = `, num)
+}
